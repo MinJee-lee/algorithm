bb09f473cd5531980cd26c53cdc2456f6c93da3b
black7amanda@gmail.com <black7amanda@gmail.com>
2021-04-11 18:30:57 +0900

    gogo

---
+++ algorithm/Roman to Integer.py
@@ -0,0 +1,11 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+   
+        dic = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        x = len(s)
+        result = dic[s[x-1]]
+        for i in range(x-1,0,-1):
+            current = dic[s[i]]
+            y = dic[s[i-1]]
+            result += y if y >= current else -y
+        return result 
