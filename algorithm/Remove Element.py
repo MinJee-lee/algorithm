05bbc80098925323768f1ca3dba810e307d7ce0a
black7amanda@gmail.com <black7amanda@gmail.com>
2021-05-07 13:32:05 +0900

    gogo

--- algorithm/Remove Element.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        idx = 0
-        while nums.count(val):
-            nums.remove(val)
-        print(nums)
-        return len(nums)
